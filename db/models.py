b52d5bb3ffd1fd4cbdf1cbc147a4bbc1cafdde77
Yevhen <evgeniy.playid@icloud.com>
2024-04-19 13:59:23 +0300

    Remove redundant "nullable" parameters

--- db/models.py
+++ db/models.py
@@ -8,9 +8,9 @@ class Book(Base):
     __tablename__ = "books"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True, nullable=False)
-    summary = Column(String, nullable=True)
-    publication_date = Column(Date, nullable=True)
+    title = Column(String, index=True)
+    summary = Column(String)
+    publication_date = Column(Date)
     author_id = Column(Integer, ForeignKey("authors.id"))
 
     author = relationship("Author", back_populates="books")
@@ -19,7 +19,7 @@ class Book(Base):
 class Author(Base):
     __tablename__ = "authors"
 
-    id = Column(Integer, primary_key=True, index=True, nullable=False)
-    name = Column(String, unique=True, index=True, nullable=False)
-    bio = Column(String, nullable=True)
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    bio = Column(String)
     books = relationship("Book", back_populates="author")
